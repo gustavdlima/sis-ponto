0506d2f530c7014d6e54c9f3bac8ca77e827df5c
gustavo <gustavdlima@gmail.com>
2023-11-22 02:12:18 -0300

    feat: no we can print date on console

---
+++ frontend/src/components/RealTimeClock.vue
@@ -0,0 +1,21 @@
+<template>
+	<div>
+		<p>{{ currentTime.toLocaleString() }}</p>
+		<p>{{ currentTime.toLocaleTimeString() }}</p>
+		<p> {{ currentTime.toLocaleDateString() }}</p>
+	</div>
+</template>
+
+<script>
+	import { useCurrentTime } from "../composable/currentTime.js";
+
+	export default {
+		name: "CurrentTimeNow",
+		setup() {
+			const { currentTime } = useCurrentTime();
+			console.log(currentTime.value)
+			return { currentTime };
+		}
+	}
+</script>
+
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,10 +1,16 @@
 <template>
 	<h1>Home</h1>
 	<router-link to="/login"></router-link>
+	<RealTimeClock></RealTimeClock>
 </template>
 
 <script>
+	import RealTimeClock from "../components/RealTimeClock.vue"
+
 	export default {
-		name: "Home"
+		name: "Home",
+		components: {
+			RealTimeClock: RealTimeClock
+		}
 	}
 </script>
