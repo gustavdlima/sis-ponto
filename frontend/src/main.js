6fedadf7dd0fd072ac8c5675183cf78e6e47c192
gustavdlima <gustavdlima@gmail.com>
2023-12-06 02:26:02 -0300

    build: add pinia

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,8 +4,13 @@ import { createApp } from 'vue'
 import router from './router/index.js'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import { createPinia } from 'pinia'
+
+
+const pinia = createPinia()
 
 createApp(App)
 	.use(router)
 	.use(VueAxios, axios)
+	.use(pinia)
 	.mount('#app')
