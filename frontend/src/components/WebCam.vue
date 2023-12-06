04c3ad93101cd7bbdd57bcf3d4de159ddd9ac2f5
gustavdlima <gustavdlima@gmail.com>
2023-12-06 16:46:18 -0300

    feat: add button WebCam

--- frontend/src/components/WebCam.vue
+++ frontend/src/components/WebCam.vue
@@ -1,18 +1,21 @@
 <template>
-	<h1>WebCam</h1>
+	<h1>WebCam Component</h1>
 	<div>
 		<video ref="video" autoplay playsinline webkit-playsinline muted hidden></video>
+		<canvas ref="canvas" width="480" height="320"></canvas>
 
-		<canvas ref="canvas" width="1280" height="720"></canvas>
+		<div>
+			<button @click="TakePicture()">Tirar foto</button>
+		</div>
 	</div>
 </template>
 
 <script setup>
 import {ref, onMounted} from 'vue';
+
 const canvas = ref(null);
 const video = ref(null);
 const ctx = ref(null);
-
 const constraints = ref({
 	audio: false,
 	video: true
@@ -34,6 +37,7 @@ function SetStream (stream) {
 	video.value.srcObject = stream;
 	video.value.play();
 
+	//The requestAnimationFrame() method tells the browser to call an animation function and repeat it very fast, forever.
 	requestAnimationFrame(Draw);
 }
 
@@ -41,5 +45,13 @@ function Draw() {
 	ctx.value.drawImage(video.value, 0, 0, canvas.value.width, canvas.value.height);
 
 	requestAnimationFrame(Draw);
+}
+
+function TakePicture() {
+	var link = document.createElement('a');
+	link.download = 'foto.png';
+	link.href = canvas.value.toDataURL();
+	link.click();
+	console.log("TIRAMO FOTO");
 }
 </script>
