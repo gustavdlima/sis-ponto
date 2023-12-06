0676e9e0e5259f07be3364c129c3fe9da5e96df0
gustavdlima <gustavdlima@gmail.com>
2023-12-06 16:47:01 -0300

    feat: baterPonto method added on Login view

--- frontend/src/views/BaterPonto.vue
+++ frontend/src/views/BaterPonto.vue
@@ -2,20 +2,15 @@
 	<div>
 		<h1>Bater Ponto</h1>
 		<a> {{ this.funcionario }}</a>
-		<WebCam />
 	</div>
 </template>
 
 <script>
 import { useFuncionarioStore } from '../stores/funcionarioStore';
 import { ref } from 'vue';
-import WebCam from '../components/WebCam.vue';
 
 export default {
 	name: 'BaterPonto',
-	components: {
-		WebCam
-	},
 	data() {
 		return {
 			funcionario: ""
--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,55 +1,3 @@
-<script>
-import BaterPonto from '../views/BaterPonto.vue'
-import router from '../router'
-import { useFuncionarioStore } from '../stores/funcionarioStore'
-
-export default {
-  name: 'Login',
-  data() {
-    return {
-      input: {
-        matricula: "",
-        data_nascimento: ""
-      },
-    }
-  },
-  methods: {
-    login() {
-      const store = useFuncionarioStore();
-      if (this.input.matricula != "" || this.input.data_nascimento != "") {
-        console.log("Autenticado")
-        console.log(this.input.matricula)
-        console.log(this.input.data_nascimento)
-        this.$http.post("http://localhost:8000/api/login", this.input)
-          .then(function (response) {
-            if (response.data) {
-              if(response.data.length) {
-                console.log("Conteudo response.data = ", response.data);
-                store.setFuncionario(response.data);
-                console.log("Pinia store: ", store.getFuncionarios);
-                router.push({
-                  name: 'BaterPonto',
-                  params: {
-                  }
-                });
-
-              } else {
-                alert("Matrícula ou Data de Nascimento errada");
-              }
-              // console.log(response);
-            }
-          })
-          .catch(function (error) {
-            console.log(error.response.data);
-          });
-      } else {
-        console.log("Complete os campos de obrigatórios")
-      }
-    }
-  }
-}
-</script>
-
 <template>
   <router-link to="/">Home</router-link>
   <form name="login-form">
@@ -64,7 +12,60 @@ export default {
     <button type="submit" v-on:click.prevent="login()">
       Login
     </button>
-    <p>Matricula is: {{ input.matricula }}</p>
-    <p>Data de Nascimento is: {{ input.data_nascimento }}</p>
   </form>
 </template>
+
+<script>
+import router from '../router'
+import { useFuncionarioStore } from '../stores/funcionarioStore'
+
+export default {
+  name: 'Login',
+  data() {
+    return {
+      input: {
+        matricula: "",
+        data_nascimento: ""
+      },
+    }
+  },
+  methods: {
+
+    baterPonto()  {
+      const currentTime = new Date();
+      console.log("Current Time: ", currentTime.toLocaleTimeString());
+      console.log("Current Date", currentTime.toLocaleDateString());
+    },
+    login() {
+      const store = useFuncionarioStore();
+      if (this.input.matricula != "" || this.input.data_nascimento != "") {
+        console.log("Autenticado")
+        console.log(this.input.matricula)
+        console.log(this.input.data_nascimento)
+        this.$http.post("http://localhost:8000/api/login", this.input)
+          .then((response) => {
+            if (response.data) {
+              if(response.data.length) {
+                console.log("Conteudo response.data = ", response.data);
+                store.setFuncionario(response.data);
+                console.log("Pinia store: ", store.getFuncionarios);
+                this.baterPonto();
+              } else {
+                alert("Matrícula ou Data de Nascimento errada");
+              }
+            }
+          })
+          .catch(function (err) {
+            console.log(err);
+          });
+      } else {
+        console.log("Complete os campos de obrigatórios")
+      }
+    },
+
+
+
+  },
+}
+</script>
+
