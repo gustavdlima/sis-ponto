ae9bd3a1eb0b4b478ff056c477f133709232dc41
gustavdlima <gustavdlima@gmail.com>
2024-01-04 09:40:24 -0300

    feat: add SideBar component

---
+++ frontend/src/components/SideBar.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class="container">
+		<ul class="list-group mt-1">
+			<li class="list-group-item m-1" role="button" @click="redirectToCadastro">Cadastrar Funcionário</li>
+			<li class="list-group-item m-1" role="button" @click="redirectToListarFuncionariosOnClick" >Listar Funcionários</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+
+export default {
+	name: "SideBar",
+	methods: {
+		redirectToListarFuncionariosOnClick() {
+			this.$router.push({ path: '/listarfuncionarios' });
+		},
+
+		redirectToCadastro() {
+			this.$router.push({ path: '/cadastro' });
+		}
+	}
+}
+
+</script>
