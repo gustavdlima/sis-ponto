8ae55771c21cd49fb997e9016e64c1d5609d1bd4
gustavdlima <gustavdlima@gmail.com>
2024-01-05 10:11:17 -0300

    feat: add new columns to funcionarioFactory

--- backend/database/factories/FuncionarioFactory.php
+++ backend/database/factories/FuncionarioFactory.php
@@ -22,8 +22,24 @@ class FuncionarioFactory extends Factory
             'matricula' => $this->faker->word,
             'nivel' => $this->faker->numberBetween(1 , 3),
             'data_nascimento' => $this->faker->date,
+            'rg' => $this->faker->word,
+            'cpf' => $this->faker->word,
+            'pis_pasep' => $this->faker->word,
+            'titulo_eleitor' => $this->faker->word,
+            'cartao_sus' => $this->faker->word,
+            'mae' => $this->faker->word,
+            'pai' => $this->faker->word,
+            'celular' => $this->faker->word,
+            'bairro' => $this->faker->word,
+            'rua' => $this->faker->word,
+            'numero' => $this->faker->word,
+            'cidade' => $this->faker->word,
+            'uf' => $this->faker->word,
+            'cep' => $this->faker->word,
+            'estado_civil' => $this->faker->word,
+            'email' => $this->faker->word,
+            'id_cargo' => $this->faker->numberBetween(1 , 10),
             'id_horario' => $this->faker->numberBetween(1 , 10),
-
         ];
     }
 }
