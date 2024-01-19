6a9b6f4a6da39252381ada0c4d884be78b620f1d
gustavdlima <gustavdlima@gmail.com>
2024-01-18 21:06:15 -0300

    refactor: put Ponto and users controllers to the respective endpoints

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,11 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\LoginController;
-use App\Http\Controllers\CadastroController;
+use App\Http\Controllers\PontoController;
+use App\Http\Controllers\CadastroFuncionarioController;
 use App\Http\Controllers\FuncionarioController;
 use App\Http\Controllers\CargoController;
 use App\Http\Controllers\HorarioController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -33,6 +34,8 @@ Route::resource('/horarios', HorarioController::class)->only(['index', 'create',
 // Route::post('/cargo', CargoController::class, 'create');
 // Route::get('/cargo/{$id}', CargoController::class, 'show');
 
-Route::post('/login', [LoginController::class, 'criarTabelaRegistro']);
+Route::post('/login', [PontoController::class, 'criarTabelaRegistro']);
 
-Route::post('/cadastro', [CadastroController::class, 'store']);
+Route::get('/users', [UserController::class, 'index']);
+Route::post('/users', [UserController::class, 'store']);
+
