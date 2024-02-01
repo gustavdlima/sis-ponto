652d3ff4de780b5b02847dcb393b4ad0c2f96d89
gustavdlima <gustavdlima@gmail.com>
2024-02-01 15:15:11 -0300

    style: admin pages stylized

--- frontend/src/views/AdminPanel.vue
+++ frontend/src/views/AdminPanel.vue
@@ -1,12 +1,12 @@
 <template>
-	<div class="container bg-warning d-flex justify-content-center align-items-center" style="height: 100vh">
-		<div class="row bg-dark h-75 w-100">
-			<div class="h-80 row-sm col-md-3 bg-danger">
-					<SideBar></SideBar>
-			</div>
-			<div class="row-sm col-md bg-info">
+	<div class="row h-100 w-100">
+		<div class="">
+				<div class="col-md-2 h-100">
+						<SideBar></SideBar>
+				</div>
+				<div class="col-md-8" >
 
-			</div>
+				</div>
 		</div>
 	</div>
 </template>
