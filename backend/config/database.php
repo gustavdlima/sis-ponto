ac351f37b8bd872da51430728558174b3f13ad00
gustavdlima <gustavdlima@gmail.com>
2024-02-05 11:25:00 -0300

    feat: change database schema to ponto

--- backend/config/database.php
+++ backend/config/database.php
@@ -74,7 +74,7 @@ return [
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
-            'search_path' => 'public',
+            'search_path' => 'ponto',
             'sslmode' => 'prefer',
             'schema' => 'ponto',
         ],
