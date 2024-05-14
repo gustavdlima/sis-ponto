f2f0c7d74ac0ebc4d1f52e6268e97c4fc92a80c7
gustavdlima <gustavdlima@gmail.com>
2024-05-14 15:22:49 -0300

    refactor: mudança no retorno da validação do funcionario

--- backend/app/Http/Controllers/PontoController.php
+++ backend/app/Http/Controllers/PontoController.php
@@ -11,21 +11,20 @@ class PontoController extends Controller
 {
     public function check(Request $request)
     {
-        // checa se o funcionário existe
         $funcionario = Funcionario::firstOrNew(['matricula' => $request->matricula]);
 
         if ($funcionario->id != null)
             return $funcionario;
         return null;
-    } 
+    }
 
     public function criarTabelaRegistro(Request $request)
     {
         // checar se o funcionário existe
         $funcionario = $this->check($request);
 
-        if ($funcionario->id == null)
-            return "Funcionário não existe";
+        if (!$funcionario)
+           return response()->json(['message' => 'Funcionário não encontrado', 'status' => '404'], 404);
 
         // criar registro de ponto e registrar o primeiro ponto
         $registro = new RegistroController();
