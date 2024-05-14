b2f6327a8aca277472b6b5b95888ee2f2ccccafb
gustavdlima <gustavdlima@gmail.com>
2024-05-14 15:31:22 -0300

    refactor: refatorização e componentização dos elementos renderizados na tela Home

---
+++ frontend/src/views/0Home.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="h-full w-full grid grid-rows-6">
+		<BotaoAdminLogin />
+		<div class="text-white overflow-hidden grid justify-center content-center row-span-5">
+			<RegistrarPonto />
+		</div>
+		<div class="row-span-1">
+			<LogoGovernoPbFooter />
+		</div>
+	</div>
+</template>
+
+<script setup>
+import BotaoAdminLogin from "../components/BotaoAdminLogin.vue"
+import RegistrarPonto from "../components/RegistrarPonto.vue"
+import LogoGovernoPbFooter from "../components/LogoGovernoPbFooter.vue"
+
+</script>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,15 +1,16 @@
 <template>
 	<div class="position-absolute top-0 end-0">
 		<button type="submit"
-						class="row-4 row-sm-2 row-md-2 btn m-3 d-flex justify-content-center text-white border-white font-weight-bold"
-						v-on:click="loginComoAdministrador()">Login</button>
+			class="row-4 row-sm-2 row-md-2 btn m-3 d-flex justify-content-center text-white border-white font-weight-bold"
+			v-on:click="loginComoAdministrador()">Login</button>
 	</div>
 	<div class="container h-100 d-flex justify-content-center align-items-center">
 		<div class="col">
 			<div class="row">
 				<div class="row">
 					<div class="col d-flex justify-content-center">
-						<img src="../assets/logo-funad-sem-nome.png" class="img-fluid col" style="max-width: 15vh;" alt="logo">
+						<img src="../assets/logo-funad-sem-nome.png" class="img-fluid col" style="max-width: 15vh;"
+							alt="logo">
 					</div>
 				</div>
 				<div class="row">
@@ -19,7 +20,7 @@
 				</div>
 			</div>
 			<div class="row">
-				<RealTimeClock class="d-flex justify-content-center" ></RealTimeClock>
+				<HoraEData class="d-flex justify-content-center"></HoraEData>
 			</div>
 			<div class="row">
 				<RegistroPonto></RegistroPonto>
@@ -32,7 +33,7 @@
 </template>
 
 <script setup>
-import RealTimeClock from "../components/RealTimeClock.vue"
+import HoraEData from "../components/HoraEData.vue"
 import RegistroPonto from "../components/RegistroPonto.vue"
 import { useRouter } from 'vue-router';
 import router from '../router';
@@ -48,5 +49,4 @@ h1 {
 	color: aliceblue !important;
 	font-weight: bold;
 }
-
 </style>
