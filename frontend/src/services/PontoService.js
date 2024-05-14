b90a47650dbf3d861576a9a878606ba6a536809e
gustavdlima <gustavdlima@gmail.com>
2024-05-14 15:29:30 -0300

    feat: serviço para componente RegistrarPonto

---
+++ frontend/src/services/PontoService.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+class PontoService {
+  constructor() {
+    this.registrarPontoURL = "http://localhost:8000/api/ponto";
+    this.getPontoDoDiaURL = "http://localhost:8000/api/registroDoDia";
+  }
+
+  async registrarPonto(matricula) {
+    try {
+      const response = await axios.post(this.registrarPontoURL, matricula);
+      return response;
+    } catch (error) {
+      return error.response.data;
+    }
+  }
+
+  async getPontoDoDia(matricula) {
+    try {
+      const response = await axios.post(this.getPontoDoDiaURL, matricula);
+      return response;
+    } catch (error) {
+      return error.response.data;
+    }
+  }
+}
+
+export default new PontoService();
