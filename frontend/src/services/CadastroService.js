4e90693094017873dec808d7f1f0039a83507cad
gustavdlima <gustavdlima@gmail.com>
2024-05-28 14:24:35 -0300

    feat: CadastroService criado para lidar com as requests relacionado a Cadastro

---
+++ frontend/src/services/CadastroService.js
@@ -0,0 +1,53 @@
+import axios from "axios";
+import { useAuthStore } from "../stores/authStore";
+
+const authStore = useAuthStore();
+
+class CadastroService {
+  constructor() {
+    this.cadastroFuncionarioURL = "http://localhost:8000/api/funcionarios";
+    this.horarioURL = "http://localhost:8000/api/horarios";
+    this.cargoURL = "http://localhost:8000/api/cargos";
+  }
+
+  async cadastrarFuncionario(data) {
+    try {
+      axios.defaults.headers.common[
+        "Authorization"
+      ] = `Bearer ${authStore.token}`;
+      const response = await axios.post(this.cadastroFuncionarioURL, data);
+      return response;
+    } catch (error) {
+      const errorResponse = JSON.parse(JSON.stringify(error.response));
+      return errorResponse;
+    }
+  }
+
+  async getHorarios() {
+    try {
+      axios.defaults.headers.common[
+        "Authorization"
+      ] = `Bearer ${authStore.token}`;
+      const response = await axios.get(this.horarioURL);
+      return response;
+    } catch (error) {
+      const errorResponse = JSON.parse(JSON.stringify(error.response));
+      return errorResponse;
+    }
+  }
+
+  async getCargos() {
+    try {
+      axios.defaults.headers.common[
+        "Authorization"
+      ] = `Bearer ${authStore.token}`;
+      const response = await axios.get(this.cargoURL);
+      return response;
+    } catch (error) {
+      const errorResponse = JSON.parse(JSON.stringify(error.response));
+      return errorResponse;
+    }
+  }
+}
+
+export default new CadastroService();
