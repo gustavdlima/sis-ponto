2c94926dcf0d6d5733f2a7cf5252d4e64a1ab528
gustavdlima <gustavdlima@gmail.com>
2024-05-31 10:50:10 -0300

    style: mudança no grid para ficar de acordo com as outras views do painel de admin

--- frontend/src/views/AdminPanel.vue
+++ frontend/src/views/AdminPanel.vue
@@ -1,11 +1,11 @@
 <template>
 	<div class="grid h-full w-full m-auto">
-		<div class="grid grid-rows-12">
-			<div class="row-span-2">
+		<div class="grid grid-rows-12 p-2">
+			<div class="row-span-1 md:row-span-2">
 				<AdminNavBar />
 			</div>
-			<div class="row-span-10">
-
+			<div class="row-span-11 md:row-span-10 p-1">
+				
 			</div>
 		</div>
 	</div>
