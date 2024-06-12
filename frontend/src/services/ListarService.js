b66c6a78f012d7f48bd0ef767274e4bd8f51fe37
gustavdlima <gustavdlima@gmail.com>
2024-06-12 11:48:49 -0300

    feat: ListarService contendo funções de requisição ao backend adicionado

--- frontend/src/services/ListarService.js
+++ frontend/src/services/ListarService.js
@@ -4,28 +4,83 @@ import { useAuthStore } from "../stores/authStore";
 const authStore = useAuthStore();
 
 class ListarService {
-	constructor() {
-		this.listarFuncionarioURL = "http://localhost:8000/api/funcionarios";
-		this.listarOperadorURL = "http://localhost:8000/api/users";
-		this.listarHorarioURL = "http://localhost:8000/api/horarios";
-		this.listarCargoURL = "http://localhost:8000/api/cargos";
-		this.listarJustificativaURL = "http://localhost:8000/api/justificativas";
-		this.horarioURL = "http://localhost:8000/api/horarios";
-		this.cargoURL = "http://localhost:8000/api/cargos";
-	}
+  constructor() {
+    this.listarFuncionarioURL = "http://localhost:8000/api/funcionarios";
+    this.registroFuncionarioURL =
+      "http://localhost:8000/api/registroFuncionario";
+    this.relatorioMensalURL = "http://localhost:8000/api/relatorio";
+    this.listarOperadorURL = "http://localhost:8000/api/users";
+    this.listarHorarioURL = "http://localhost:8000/api/horarios";
+    this.listarCargoURL = "http://localhost:8000/api/cargos";
+    this.listarJustificativaURL = "http://localhost:8000/api/justificativas";
+    this.horarioURL = "http://localhost:8000/api/horarios";
+    this.cargoURL = "http://localhost:8000/api/cargos";
+    this.justificarFaltaURL = "http://localhost:8000/api/faltas";
+  }
 
-	async listarFuncionarios() {
-		try {
-			axios.defaults.headers.common
-				["Authorization"] = `Bearer ${authStore.token}`;
-			const response = await axios.get(this.listarFuncionarioURL);
-			return response;
-		} catch (error) {
-			const errorResponse = JSON.parse(JSON.stringify(error.response));
-			return errorResponse;
-		}
-	}
+  async listarFuncionarios() {
+    try {
+      axios.defaults.headers.common[
+        "Authorization"
+      ] = `Bearer ${authStore.token}`;
+      const response = await axios.get(this.listarFuncionarioURL);
+      return response;
+    } catch (error) {
+      const errorResponse = JSON.parse(JSON.stringify(error.response));
+      return errorResponse;
+    }
+  }
+
+  async registroFuncionario(data) {
+    try {
+      axios.defaults.headers.common[
+        "Authorization"
+      ] = `Bearer ${authStore.token}`;
+      const response = await axios.post(this.registroFuncionarioURL, data);
+      return response;
+    } catch (error) {
+      const errorResponse = JSON.parse(JSON.stringify(error.response));
+      return errorResponse;
+    }
+  }
+
+  async relatorioMensal(data) {
+    try {
+      axios.defaults.headers.common[
+        "Authorization"
+      ] = `Bearer ${authStore.token}`;
+      const response = await axios.post(this.relatorioMensalURL, data);
+      return response;
+    } catch (error) {
+      const errorResponse = JSON.parse(JSON.stringify(error.response));
+      return errorResponse;
+    }
+  }
+
+  async justificativas() {
+    try {
+      axios.defaults.headers.common[
+        "Authorization"
+      ] = `Bearer ${authStore.token}`;
+      const response = await axios.get(this.listarJustificativaURL);
+      return response;
+    } catch (error) {
+      const errorResponse = JSON.parse(JSON.stringify(error.response));
+      return errorResponse;
+    }
+  }
+  async registrarFalta(data) {
+    try {
+      axios.defaults.headers.common[
+        "Authorization"
+      ] = `Bearer ${authStore.token}`;
+      const response = await axios.post(this.justificarFaltaURL, data);
+      return response;
+    } catch (error) {
+      const errorResponse = JSON.parse(JSON.stringify(error.response));
+      return errorResponse;
+    }
+  }
 }
-
 
 export default new ListarService();
