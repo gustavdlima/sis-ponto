17d60126a909051688210f9204358790206ef967
gustavdlima <gustavdlima@gmail.com>
2024-06-12 11:46:56 -0300

    feat: componente de tabela para mostrar todo o registro do funcionario adicionado

---
+++ frontend/src/components/Admin/Tabelas/TabelaRegistroGeral.vue
@@ -0,0 +1,28 @@
+<template>
+	<div class="grid h-full w-full m-auto">
+		<DataTable :value="registro" stripedRows showGridLines="true" dataKey="id" :paginator="true" :rows="10" :rowsPerPageOptions="[5, 15, 30]" size="small">
+			<Column field="data" header="Data"></Column>
+			<Column field="primeiro_ponto" header="Primeiro Horario"></Column>
+			<Column field="segundo_ponto" header="Segundo Horario"></Column>
+			<Column field="terceiro_ponto" header="Terceiro Horario"></Column>
+			<Column field="quarto_ponto" header="Quarto Horario"></Column>
+			<Column field="justificativa" header="Justificativa"></Column>
+		</DataTable>
+	</div>
+</template>
+
+<script setup>
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+import { ref, watch } from 'vue';
+
+const props = defineProps({
+	registro: Array
+});
+
+
+</script>
+
+<style scoped>
+
+</style>
