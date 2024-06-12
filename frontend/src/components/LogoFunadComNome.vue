b4d8d864ad267be6fcbd1dd9e7dd474379e35355
gustavdlima <gustavdlima@gmail.com>
2024-06-12 11:44:34 -0300

    feat: componente da logo da Funad com o nome Funad criado

---
+++ frontend/src/components/LogoFunadComNome.vue
@@ -0,0 +1,10 @@
+<template>
+	<div class="grid justify-center">
+		<img src="../assets/logo-funad-nome.png" class="img-fluid col" style="max-width: 10vh;"
+				alt="logo">
+	</div>
+</template>
+
+<script setup>
+
+</script>
