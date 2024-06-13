7d261cabf4a9854713a96e4eaecb85ced5245e6c
gustavdlima <gustavdlima@gmail.com>
2024-06-13 10:50:24 -0300

    feat: ListarOperadores view e TabelaOperadores componentes adicionados e funcionais

---
+++ frontend/src/components/Admin/Tabelas/Listar/TabelaOperadores.vue
@@ -0,0 +1,29 @@
+<template>
+		<div class="grid h-full w-full m-auto p-2">
+			<div class=" bg-white h-full w-full rounded-lg border-3 p-4">
+				<DataTable :value="props.operadores"
+				tableStyle="padding: 1rem;"
+				paginator :rows="10"
+				:rowsPerPageOptions="[10, 20, 30, 50]">
+					<Column field="id" header="ID" sortable></Column>
+					<Column field="name" header="Nome"></Column>
+					<Column field="email" header="Email"></Column>
+					<Column field="level" header="Nível"></Column>
+				</DataTable>
+			</div>
+		</div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+
+const props = defineProps({
+	operadores: {
+		type: Array,
+		required: true
+	}
+});
+
+</script>
--- frontend/src/views/Admin/ListarViews/ListarOperadores.vue
+++ frontend/src/views/Admin/ListarViews/ListarOperadores.vue
@@ -1,6 +1,28 @@
 <template>
-
+	<div class="grid h-full w-full m-auto">
+		<div class="grid grid-rows-12 p-2">
+			<div class="row-span-1 md:row-span-2">
+				<AdminNavBar />
+			</div>
+			<div class="row-span-11 md:row-span-10">
+				<TabelaOperadores :operadores="operadores" />
+			</div>
+		</div>
+	</div>
 </template>
 
 <script setup>
+import AdminNavBar from '../../../components/AdminNavBar.vue';
+import useListarService from '../../../services/ListarService';
+import TabelaOperadores from '../../../components/Admin/Tabelas/Listar/TabelaOperadores.vue';
+import { ref, onMounted } from 'vue';
+
+const operadores = ref();
+
+onMounted(async () => {
+	const response = await useListarService.listarOperadores();
+	console.log(response.data);
+	operadores.value = response.data;
+});
+
 </script>
