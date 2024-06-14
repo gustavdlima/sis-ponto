4deb9adc936597ab5862b89dd5744900182e63cb
gustavdlima <gustavdlima@gmail.com>
2024-06-13 21:09:08 -0300

    feat:  view ListarJustificativa e componente TabelaJustificativa agora funcionais

--- frontend/src/views/Admin/ListarViews/ListarJustificativas.vue
+++ frontend/src/views/Admin/ListarViews/ListarJustificativas.vue
@@ -1,6 +1,28 @@
 <template>
-
+	<div class="grid h-full w-full m-auto">
+		<div class="grid grid-rows-12 p-2">
+			<div class="row-span-1 md:row-span-2">
+				<AdminNavBar />
+			</div>
+			<div class="row-span-11 md:row-span-10">
+						<TabelaJustificativa :justificativa="justificativas" />
+			</div>
+		</div>
+	</div>
 </template>
 
 <script setup>
+import AdminNavBar from '../../../components/AdminNavBar.vue';
+import useListarService from '../../../services/ListarService';
+import { ref, onMounted } from 'vue';
+import TabelaJustificativa from '../../../components/Admin/Tabelas/Listar/TabelaJustificativa.vue';
+
+const justificativas = ref([]);
+
+onMounted(async () => {
+	const response = await useListarService.listarJustificativas();
+	justificativas.value = response.data;
+	console.log(justificativas.value);
+});
+
 </script>
