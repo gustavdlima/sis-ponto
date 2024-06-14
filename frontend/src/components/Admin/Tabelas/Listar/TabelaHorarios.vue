de27b4a75173551940c04145a8c054d827f005cd
gustavdlima <gustavdlima@gmail.com>
2024-06-13 21:07:13 -0300

    feat:  view ListarHorarios e componente TabelaHorarios agora funcionais

---
+++ frontend/src/components/Admin/Tabelas/Listar/TabelaHorarios.vue
@@ -0,0 +1,28 @@
+<template>
+	<div class="grid h-full w-full m-auto p-2">
+		<div class=" bg-white h-full w-full rounded-lg border-3 p-4">
+			<DataTable :value="props.horarios" tableStyle="padding: 1rem;" paginator :rows="10"
+				:rowsPerPageOptions="[10, 20, 30, 50]">
+				<Column field="id" header="ID" sortable></Column>
+				<Column field="primeiro_horario" header="Primeiro Horário"></Column>
+				<Column field="segundo_horario" header="Segundo Horário"></Column>
+				<Column field="terceiro_horario" header="Terceiro Horário"></Column>
+				<Column field="quarto_horario" header="Quarto Horário"></Column>
+			</DataTable>
+		</div>
+	</div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+
+const props = defineProps({
+	horarios: {
+		type: Array,
+		required: true
+	}
+});
+
+</script>
--- frontend/src/views/Admin/ListarViews/ListarHorarios.vue
+++ frontend/src/views/Admin/ListarViews/ListarHorarios.vue
@@ -1,6 +1,28 @@
 <template>
-
+	<div class="grid h-full w-full m-auto">
+		<div class="grid grid-rows-12 p-2">
+			<div class="row-span-1 md:row-span-2">
+				<AdminNavBar />
+			</div>
+			<div class="row-span-11 md:row-span-10">
+				<TabelaHorarios :horarios="horarios" />
+			</div>
+		</div>
+	</div>
 </template>
 
 <script setup>
+import AdminNavBar from '../../../components/AdminNavBar.vue';
+import useListarService from '../../../services/ListarService';
+import TabelaHorarios from '../../../components/Admin/Tabelas/Listar/TabelaHorarios.vue';
+import { ref, onMounted } from 'vue';
+
+const horarios = ref();
+
+onMounted(async () => {
+	const response = await useListarService.listarHorarios();
+	horarios.value = response.data;
+	console.log(response.data);
+});
+
 </script>
