c94aa85f7af3626f80789c72026b06b712d914fc
gustavdlima <gustavdlima@gmail.com>
2024-06-13 21:06:58 -0300

    feat:  view ListarCargos e TabelaCargos agora funcionais

---
+++ frontend/src/components/Admin/Tabelas/Listar/TabelaCargos.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class="grid h-full w-full m-auto p-2">
+		<div class=" bg-white h-full w-full rounded-lg border-3 p-4">
+			<DataTable :value="props.cargos" tableStyle="padding: 1rem;" paginator :rows="10"
+				:rowsPerPageOptions="[10, 20, 30, 50]">
+				<Column field="id" header="ID" sortable></Column>
+				<Column field="cargo" header="Cargo" ></Column>
+			</DataTable>
+		</div>
+	</div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+
+const props = defineProps({
+	cargos: {
+		type: Array,
+		required: true
+	}
+});
+
+</script>
--- frontend/src/views/Admin/ListarViews/ListarCargos.vue
+++ frontend/src/views/Admin/ListarViews/ListarCargos.vue
@@ -1,6 +1,28 @@
 <template>
-
+	<div class="grid h-full w-full m-auto">
+		<div class="grid grid-rows-12 p-2">
+			<div class="row-span-1 md:row-span-2">
+				<AdminNavBar />
+			</div>
+			<div class="row-span-11 md:row-span-10">
+					<TabelaCargos :cargos="cargos" />
+			</div>
+		</div>
+	</div>
 </template>
 
 <script setup>
+import AdminNavBar from '../../../components/AdminNavBar.vue';
+import useListarService from '../../../services/ListarService';
+import TabelaCargos from '../../../components/Admin/Tabelas/Listar/TabelaCargos.vue';
+import { ref, onMounted } from 'vue'
+
+const cargos = ref([]);
+
+onMounted(async () => {
+	const response = await useListarService.listarCargos();
+	cargos.value = response.data;
+	console.log(cargos.value)
+});
+
 </script>
