1fe46f90f514bac04c57d78c4b585a370e5ac80f
gustavdlima <gustavdlima@gmail.com>
2024-06-21 17:43:14 -0300

    feat: botão e dialogo para editar cargo na tabela de cargos adicionados e funcionais

---
+++ frontend/src/components/Admin/Dialog/EditarCargosDialog.vue
@@ -0,0 +1,101 @@
+<template>
+	<Dialog v-model:visible="dialogEditarCargoIsVisible" header="Editar Cargo"
+		@update:visible="fecharEditarCargoDialog" :modal="true" :closable="true" :style="{ width: '95vw' }">
+		<div class="grid h-full w-full">
+			<form>
+			<div class="grid h-full w-full grid-rows-1 py-20 lg:px-3 justify-center">
+				<div class="row-span-1">
+					<div class="grid content-center grid-cols-1">
+						<div class="col-span-1 p-1">
+							<InputText v-model="cargo.cargo" class="w-full lg:w-[16.6rem] h-[2.5rem]"
+								placeholder="Cargo / Função" />
+						</div>
+					</div>
+					<div class="grid grid-rows-1 justify-center mt-8">
+						<Button
+							class="border-2 bg-transparent h-8 w-32 md:w-36 lg:w-40 lg:h-10 text-xs md:text-sm lg:text-lg button mt-2"
+							label="Editar" severity="info" raised @click="enviarFormularioDeEdicaoDeCargo()" />
+					</div>
+				</div>
+			</div>
+		</form>
+		</div>
+	</Dialog>
+
+	<DialogAlerta :conteudoPropsDialog="{ dialogMensagem: dialogMensagem, dialogVisivel: dialogVisivel }" />
+</template>
+
+<script setup>
+import { ref, watch } from 'vue';
+import Button from 'primevue/button';
+import Dialog from 'primevue/dialog';
+import InputText from 'primevue/inputtext';
+import useEditarService from '../../../services/EditarService'
+import useUtils from '../../../services/Utils'
+import DialogAlerta from '../../DialogAlerta.vue';
+
+const props = defineProps({
+	isVisible: Boolean,
+	cargo: Object
+});
+
+const dialogEditarCargoIsVisible = ref();
+const cargoEditado = ref();
+const dialogMensagem = ref();
+const dialogVisivel = ref();
+const emit = defineEmits(['atualizarDialogEditarCargoBool']);
+
+const enviarFormularioDeEdicaoDeCargo = async () => {
+	if (cargoEditado.value.cargo === "") {
+		dialogMensagem.value = "Campo vazio!";
+		dialogVisivel.value = true;
+		useUtils.sleep(1000).then(() => {
+			dialogVisivel.value = false;
+		});
+		return;
+	}
+	const response = await useEditarService.editarCargo(cargoEditado.value)
+	console.log(response.data);
+	handleResponse(response);
+	fecharEditarCargoDialog();
+};
+
+const handleResponse = (response) => {
+	switch (response.status) {
+		case 200:
+			dialogMensagem.value = response.data.message;
+			dialogVisivel.value = true;
+			useUtils.sleep(2000).then(() => {
+				dialogVisivel.value = false;
+			})
+			return;
+			break;
+		default:
+			dialogMensagem.value = response.data.message;
+			dialogVisivel.value = true;
+			useUtils.sleep(2000).then(() => {
+				dialogVisivel.value = false;
+			})
+			return;
+			break;
+	}
+}
+
+const fecharEditarCargoDialog = () => {
+	dialogEditarCargoIsVisible.value = false;
+	emit('atualizarDialogEditarCargoBool', false);
+}
+
+watch(props, () => {
+	dialogEditarCargoIsVisible.value = props.isVisible;
+	cargoEditado.value = props.cargo;
+});
+
+</script>
+
+<style scoped>
+button {
+	background-color: #1F2937 !important;
+	border: none !important
+}
+</style>
--- frontend/src/components/Admin/Tabelas/Listar/TabelaCargos.vue
+++ frontend/src/components/Admin/Tabelas/Listar/TabelaCargos.vue
@@ -5,15 +5,28 @@
 				:rowsPerPageOptions="[10, 20, 30, 50]">
 				<Column field="id" header="ID" sortable></Column>
 				<Column field="cargo" header="Cargo" ></Column>
+				<Column field="" header="">
+					<template #body="slotProps">
+						<Button class="botao-opcoes h-9 w-12 text-sm md:text-md"
+										@click="abrirEditarCargosDialog(slotProps)">
+										<i class="pi pi-w pi-pen-to-square" style="font-size: 1.8rem"></i>
+						</Button>
+					</template>
+				</Column>
 			</DataTable>
 		</div>
 	</div>
+
+	<DialogEditarCargos :cargo="cargoSelecionado" :isVisible="dialogEditarCargoIsVisible" @atualizarDialogEditarCargoBool="atualizarValorEditarCargoBool" />
+
 </template>
 
 <script setup>
 import { ref } from 'vue';
 import DataTable from 'primevue/datatable';
 import Column from 'primevue/column';
+import Button from 'primevue/button';
+import DialogEditarCargos from '../../Dialog/EditarCargosDialog.vue';
 
 const props = defineProps({
 	cargos: {
@@ -21,5 +34,40 @@ const props = defineProps({
 		required: true
 	}
 });
+const dialogEditarCargoIsVisible = ref(false);
+const cargoSelecionado = ref();
+
+const abrirEditarCargosDialog = (slotProps) => {
+	cargoSelecionado.value = slotProps.data;
+	dialogEditarCargoIsVisible.value = true;
+	console.log(slotProps);
+}
+
+const atualizarValorEditarCargoBool = (eventData) => {
+	dialogEditarCargoIsVisible.value = eventData.dialogEditarCargoIsVisible;
+}
 
 </script>
+
+<style scoped>
+p-datatable-header {
+	padding: 1rem;
+}
+
+i {
+	color: #1F2937 !important;
+}
+
+i:hover {
+	color: aliceblue !important;
+}
+
+.botao-opcoes:hover {
+	background-color: #1F2937 !important;
+}
+
+button {
+	background-color: transparent !important;
+	border: none !important;
+}
+</style>
