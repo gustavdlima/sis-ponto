becc1c33f84466efdbe20d50cbe7cd018775bc7c
gustavdlima <gustavdlima@gmail.com>
2024-06-30 18:00:38 -0300

    chore: migration para adicionar id da tabela de dia de semana

--- backend/database/migrations/2024_06_28_193658_add_sexo_IdDiasDaSemana_deficiente_col_to_funcionario.php
+++ backend/database/migrations/2024_06_28_193658_add_sexo_IdDiasDaSemana_deficiente_col_to_funcionario.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::table('funcionarios', function (Blueprint $table) {
             $table->string('sexo')->nullable();
             $table->boolean('deficiente')->nullable();
+            $table->integer('id_dia_da_semana')->nullable();
         });
     }
 
