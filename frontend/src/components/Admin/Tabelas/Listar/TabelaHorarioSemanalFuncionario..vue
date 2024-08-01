b1691149c449aa0d3c69ff90603438cf07aa1206
gustavdlima <gustavdlima@gmail.com>
2024-08-01 16:01:25 -0300

    feat: componente que mostra uma tabela contendo o horario semanal do funcionário adicionado

---
+++ frontend/src/components/Admin/Tabelas/Listar/TabelaHorarioSemanalFuncionario..vue
@@ -0,0 +1,23 @@
+<template>
+		<div class="grid max-h-40">
+			<DataTable columnResizeMode="fit" showGridlines :value="props.arrayHorarioSemanal" tableStyle="width: 30rem; height: 17rem;">
+
+				<Column field="segunda.horario" header="Segunda"></Column>
+				<Column field="terca.horario" header="Terça"></Column>
+				<Column field="quarta.horario" header="Quarta"></Column>
+				<Column field="quinta.horario" header="Quinta"></Column>
+				<Column field="sexta.horario" header="Sexta"></Column>
+			</DataTable>
+		</div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+
+const props = defineProps({
+		arrayHorarioSemanal: Array,
+		funcionario: Object
+	});
+</script>
