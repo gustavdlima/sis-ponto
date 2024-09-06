a05ef859e06acb86cffdb93a85ff3bc952ae556f
gustavdlima <gustavdlima@gmail.com>
2024-09-06 14:37:01 -0300

    feat:  relatorio mensal do usuario consegue monitorar as justificativas de turnos especificos ou totais

--- backend/app/Http/Controllers/RegistroController.php
+++ backend/app/Http/Controllers/RegistroController.php
@@ -11,7 +11,8 @@ use App\Models\Justificativa;
 class RegistroController extends Controller
 {
 
-    public function converterDiaDaSemanaParaPortugues($diaDaSemana) {
+    public function converterDiaDaSemanaParaPortugues($diaDaSemana)
+    {
         switch ($diaDaSemana) {
             case 'Sunday':
                 return 'Domingo';
@@ -30,31 +31,16 @@ class RegistroController extends Controller
         }
     }
 
-    public function tratarDadosRelatorio($funcionario, $relatorio) {
+    public function tratarDadosRelatorio($funcionario, $relatorio)
+    {
+        $relatorio = $this->inserirJustificativaNoRelatorio($relatorio);
 
         for ($i = 0; $i < sizeof($relatorio); $i++) {
-
-            // adiciona Justificado no campo do ponto no relatório
-            if ($relatorio[$i]['justificativa'] && $relatorio[$i]['registroDoDia']) {
-                if (!isset($relatorio[$i]['registroDoDia'][0]['primeiro_ponto']) || $relatorio[$i]['registroDoDia'][0]['primeiro_ponto'] == 'Sem Registro') {
-                    $relatorio[$i]['registroDoDia'][0]['primeiro_ponto'] = 'JUSTIFICADO';
-                }
-                if (!isset($relatorio[$i]['registroDoDia'][0]['segundo_ponto'])) {
-                    $relatorio[$i]['registroDoDia'][0]['segundo_ponto'] = 'JUSTIFICADO';
-                }
-                if (!isset($relatorio[$i]['registroDoDia'][0]['terceiro_ponto']) && $funcionario->carga_horaria == '40h') {
-                    $relatorio[$i]['registroDoDia'][0]['terceiro_ponto'] = 'JUSTIFICADO';
-                }
-                if (!isset($relatorio[$i]['registroDoDia'][0]['quarto_ponto']) && $funcionario->carga_horaria == '40h') {
-                    $relatorio[$i]['registroDoDia'][0]['quarto_ponto'] = 'JUSTIFICADO';
-                }
-            }
-
             // se a data for um sábado ou domingo, o registro do dia é nulo
             $data = explode('/', $relatorio[$i]['dia']);
             $diaDaSemana = date('w', strtotime($data[2] . '-' . $data[1] . '-' . $data[0]));
             if ($diaDaSemana == 0 || $diaDaSemana == 6) {
-                $relatorio[$i]['registroDoDia'] = array(
+                $relatorio[$i]['registroDoDia'] = [array(
                     'primeiro_ponto' => '-',
                     'segundo_ponto' => '-',
                     'terceiro_ponto' => '-',
@@ -63,18 +49,34 @@ class RegistroController extends Controller
                     'atrasou_segundo_ponto' => false,
                     'atrasou_terceiro_ponto' => false,
                     'atrasou_quarto_ponto' => false
-                );
+                )];
             }
-            if ($relatorio[$i]['justificativa'] !== null) {
-                if (!$relatorio[$i]['justificativa'][0]['data2'])
-                    $relatorio[$i]['justificativa'][0]['data2'] = $relatorio[$i]['justificativa'][0]['data'];
-                $dataInicio = explode('-', $relatorio[$i]['justificativa'][0]['data']);
-                $dataFim = explode('-', $relatorio[$i]['justificativa'][0]['data2']);
+
+            // se o ponto for nulo, substituir por 'Sem Registro'
+            if ($relatorio[$i]['registroDoDia'][0]['primeiro_ponto'] == null)
+                $relatorio[$i]['registroDoDia'][0]['primeiro_ponto'] = 'Sem Registro';
+            if ($relatorio[$i]['registroDoDia'][0]['segundo_ponto'] == null)
+                $relatorio[$i]['registroDoDia'][0]['segundo_ponto'] = 'Sem Registro';
+            if ($relatorio[$i]['registroDoDia'][0]['terceiro_ponto'] == null)
+                $relatorio[$i]['registroDoDia'][0]['terceiro_ponto'] = 'Sem Registro';
+            if ($relatorio[$i]['registroDoDia'][0]['quarto_ponto'] == null)
+                $relatorio[$i]['registroDoDia'][0]['quarto_ponto'] = 'Sem Registro';
+
+
+            if ($relatorio[$i]['falta'] !== null) {
+                if (!$relatorio[$i]['falta'][0]['data2'])
+                    $relatorio[$i]['falta'][0]['data2'] = $relatorio[$i]['falta'][0]['data'];
+                $dataInicio = explode('-', $relatorio[$i]['falta'][0]['data']);
+                $dataFim = explode('-', $relatorio[$i]['falta'][0]['data2']);
+                $justificativa = $relatorio[$i]['justificativa'];
                 for ($j = $dataInicio[2]; $j <= $dataFim[2]; $j++) {
                     // se a data for um fim de semana, o registro do dia é nulo
                     $diaDaSemana = date('w', strtotime($dataInicio[0] . '-' . $dataInicio[1] . '-' . $j));
+                    if ($relatorio[$j - 1]['justificativa'] == null) {
+                        $relatorio[$j - 1]['justificativa'] = $justificativa;
+                    }
                     if ($diaDaSemana == 0 || $diaDaSemana == 6) {
-                        $relatorio[$j - 1]['registroDoDia'] = array(
+                        $relatorio[$j - 1]['registroDoDia'] = [array(
                             'primeiro_ponto' => '-',
                             'segundo_ponto' => '-',
                             'terceiro_ponto' => '-',
@@ -83,31 +85,31 @@ class RegistroController extends Controller
                             'atrasou_segundo_ponto' => false,
                             'atrasou_terceiro_ponto' => false,
                             'atrasou_quarto_ponto' => false
-                        );
+                        )];
                     } else {
-                        if (!isset($relatorio[$j - 1]['registroDoDia'][0]['primeiro_ponto'])) {
+                        if ($relatorio[$j - 1]['falta'][0]->primeiro_turno == true) {
                             $relatorio[$j - 1]['registroDoDia'][0]['primeiro_ponto'] = 'JUSTIFICADO';
                         }
-                        if (!isset($relatorio[$j - 1]['registroDoDia'][0]['segundo_ponto'])) {
+                        if ($relatorio[$j - 1]['falta'][0]->segundo_turno == true)
+                        {
                             $relatorio[$j - 1]['registroDoDia'][0]['segundo_ponto'] = 'JUSTIFICADO';
                         }
-                        if (!isset($relatorio[$j - 1]['registroDoDia'][0]['terceiro_ponto']) && $funcionario->carga_horaria == '40h') {
+                        if ($relatorio[$j - 1]['falta'][0]->terceiro_turno == true) {
                             $relatorio[$j - 1]['registroDoDia'][0]['terceiro_ponto'] = 'JUSTIFICADO';
                         }
-                        if (!isset($relatorio[$j - 1]['registroDoDia'][0]['quarto_ponto']) && $funcionario->carga_horaria == '40h') {
+                        if ($relatorio[$j - 1]['falta'][0]->quarto_turno == true)
+                        {
                             $relatorio[$j - 1]['registroDoDia'][0]['quarto_ponto'] = 'JUSTIFICADO';
                         }
-                        $relatorio[$j - 1]['justificativa'] = $relatorio[$i]['justificativa'];
                     }
                 }
             }
-
         }
-        $novoRelatorio = $this->inserirJustificativaNoRelatorio($relatorio);
-        return $novoRelatorio;
+        return $relatorio;
     }
 
-    public function inserirJustificativaNoRelatorio($relatorio) {
+    public function inserirJustificativaNoRelatorio($relatorio)
+    {
         for ($i = 0; $i < sizeof($relatorio); $i++) {
             if ($relatorio[$i]['justificativa']) {
                 $justificativa = Justificativa::where('id', $relatorio[$i]['justificativa'][0]['id_justificativa'])->get();
@@ -117,14 +119,16 @@ class RegistroController extends Controller
         return $relatorio;
     }
 
-    public function cadastraFaltaNoRegistro($funcionario, $data) {
+    public function cadastraFaltaNoRegistro($funcionario, $data)
+    {
         $falta = Falta::where('id_funcionario', $funcionario->id)->where('data', 'like', '%' . $data . '%')->get();
         if (count($falta) == 0)
             return null;
         return $falta;
     }
 
-    public function trataORegistroAntigoParaORelatorio($registro) {
+    public function trataORegistroAntigoParaORelatorio($registro)
+    {
         if ($registro == null)
             return null;
 
@@ -144,15 +148,26 @@ class RegistroController extends Controller
         return $registro;
     }
 
-    public function retornaORegistroDaDataEspecificada($funcionario, $data, $day) {
+    public function retornaORegistroDaDataEspecificada($funcionario, $data, $day)
+    {
         // retornar o registro do dia especifico
         $registro = Registro::where('id_funcionario', $funcionario->id)->whereMonth('created_at', date('m', strtotime($data)))->whereYear('created_at', date('Y', strtotime($data)))->whereDay('created_at', date('d', strtotime($data)))->get();
-        if (count($registro) == 0)
-            return null;
+
+        $registroNull = array(
+            'primeiro_ponto' => 'Sem Registro',
+            'segundo_ponto' => 'Sem Registro',
+            'terceiro_ponto' => 'Sem Registro',
+            'quarto_ponto' => 'Sem Registro',
+        );
+
+        if (count($registro) == 0) {
+            return [$registroNull];
+        }
         return $registro;
     }
 
-    public function tratarMesSelecionado($mesSelecionado) {
+    public function tratarMesSelecionado($mesSelecionado)
+    {
         if ($mesSelecionado != '10' || $mesSelecionado != '11' || $mesSelecionado != '12') {
             $mesSelecionado = '0' . $mesSelecionado;
         }
@@ -160,10 +175,23 @@ class RegistroController extends Controller
         return $mesSelecionado;
     }
 
+    public function retornaAFaltaDoDiaDoFuncionario($funcionario, $data)
+    {
+        $falta = Falta::where('id_funcionario', $funcionario->id)->where('data', 'like', '%' . $data . '%')->get();
+
+        if (count($falta) == 0) {
+            $falta = Falta::where('id_funcionario', $funcionario->id)->where('data2', 'like', '%' . $data . '%')->get();
+            if (count($falta) == 0)
+                return null;
+        }
+        return $falta;
+    }
+
     /**
      * Gera o relatório de registro do ponto.
      */
-    public function gerarRelatorioDeRegistroDoPonto(Request $request) {
+    public function gerarRelatorioDeRegistroDoPonto(Request $request)
+    {
         $funcionario = Funcionario::firstOrNew(['matricula' => $request->matricula]);
         if ($funcionario == null)
             return response()->json(['message' => 'Funcionário não encontrado'], 404);
@@ -189,33 +217,31 @@ class RegistroController extends Controller
 
             $registro = $this->retornaORegistroDaDataEspecificada($funcionario, $dataDB, $day);
             $relatorio[$i]['registroDoDia'] = $this->trataORegistroAntigoParaORelatorio($registro);
+
+            // buscar as faltas do funcionário
+            $relatorio[$i]['falta'] = $this->retornaAFaltaDoDiaDoFuncionario($funcionario, $dataDB);
+
             $relatorio[$i]['justificativa'] = $this->cadastraFaltaNoRegistro($funcionario, $dataDB);
         }
+        // return $relatorio;
         $relatorioTratado = $this->tratarDadosRelatorio($funcionario, $relatorio);
 
         return $relatorioTratado;
-
     }
 
     /**
      * Retorna o último registro do funcionário.
      */
-    public function retornaOUltimoRegistroDoFuncionario($funcionario) {
+    public function retornaOUltimoRegistroDoFuncionario($funcionario)
+    {
         $registro = Registro::where('id_funcionario', $funcionario->id)->latest()->first();
 
         return $registro;
     }
 
-    /**
-     * Retorna todo o registro do funcionário.
-     */
-    public function retornaTodoORegistroDoFuncionario(Request $request) {
-        $funcionario = Funcionario::findOrFail($request->id_funcionario);
-        $registro = Registro::where('id_funcionario', $funcionario->id)
-            ->orderBy('created_at', 'desc')->get();
-
+    public function converteTimestampParaHoraConvencional($registro)
+    {
         for ($i = 0; $i < count($registro); $i++) {
-
             if ($registro[$i]['primeiro_ponto'] != null && $registro[$i]['primeiro_ponto'] != 'FALTA' && $registro[$i]['primeiro_ponto'] != 'JUSTIFICATIVA' && $registro[$i]['primeiro_ponto'] != 'JUSTIFICADO' && $registro[$i]['primeiro_ponto'] != 'ATRASADO' && $registro[$i]['primeiro_ponto'] != 'Sem Registro')
                 $registro[$i]['primeiro_ponto'] = date("H:i:s", strtotime($registro[$i]['primeiro_ponto']));
             if ($registro[$i]['segundo_ponto'] != null && $registro[$i]['segundo_ponto'] != 'FALTA' && $registro[$i]['segundo_ponto'] != 'JUSTIFICATIVA' && $registro[$i]['segundo_ponto'] != 'JUSTIFICADO' && $registro[$i]['segundo_ponto'] != 'ATRASADO' && $registro[$i]['segundo_ponto'] != 'Sem Registro')
@@ -224,12 +250,30 @@ class RegistroController extends Controller
                 $registro[$i]['terceiro_ponto'] = date("H:i:s", strtotime($registro[$i]['terceiro_ponto']));
             if ($registro[$i]['quarto_ponto'] != null && $registro[$i]['quarto_ponto'] != 'FALTA' && $registro[$i]['quarto_ponto'] != 'JUSTIFICATIVA' && $registro[$i]['quarto_ponto'] != 'JUSTIFICADO' && $registro[$i]['quarto_ponto'] != 'ATRASADO' && $registro[$i]['quarto_ponto'] != 'Sem Registro')
                 $registro[$i]['quarto_ponto'] = date("H:i:s", strtotime($registro[$i]['quarto_ponto']));
+        }
+
+        return $registro;
+    }
+
+    /**
+     * Retorna todo o registro do funcionário.
+     */
+    public function retornaTodoORegistroDoFuncionario(Request $request)
+    {
+        $funcionario = Funcionario::findOrFail($request->id_funcionario);
+        $registro = Registro::where('id_funcionario', $funcionario->id)
+            ->orderBy('created_at', 'desc')->get();
+
+        $registro = $this->converteTimestampParaHoraConvencional($registro);
+
+        for ($i = 0; $i < count($registro); $i++) {
 
             $registro[$i]['data'] = date("d/m/Y", strtotime($registro[$i]['created_at']));
-            $registro[$i]['justificativa'] = $this->cadastraFaltaNoRegistro($funcionario, date("Y-m-d", strtotime($registro[$i]
-            ['created_at'])));
+            $registro[$i]['justificativa'] = $this->cadastraFaltaNoRegistro($funcionario, date("Y-m-d", strtotime($registro[$i]['created_at'])));
         }
+
         $registro = $this->inserirJustificativaNoRelatorio($registro);
+
         return $registro;
     }
     /**
