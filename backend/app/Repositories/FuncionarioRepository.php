94ae4feb8425d5a6cfb178c77bb81c057232845a
gustavdlima <gustavdlima@gmail.com>
2024-09-19 15:39:51 -0300

    refactor: separando a logica do FuncionarioController no padrão de repositório

---
+++ backend/app/Repositories/FuncionarioRepository.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Funcionario;
+
+class FuncionarioRepository
+{
+	protected $model;
+
+	public function __construct(Funcionario $model)
+	{
+		$this->model = $model;
+	}
+
+	public function all()
+	{
+		return $this->model->all();
+	}
+
+	public function create(array $data)
+	{
+		return $this->model->create($data);
+	}
+
+	public function firstOrNew(array $data)
+	{
+		return $this->model->firstOrNew($data);
+	}
+
+	public function find($id)
+	{
+		return $this->model->find($id);
+	}
+
+	public function update(array $data, $id)
+	{
+		$funcionario = $this->model->find($id);
+		$funcionario->update($data);
+		return $funcionario;
+	}
+
+	public function delete($id)
+	{
+		return $this->model->destroy($id);
+	}
+}
