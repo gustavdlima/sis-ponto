04a621bc618022ee94128c81ac7946149f28d26d
gustavdlima <gustavdlima@gmail.com>
2024-09-23 15:26:28 -0300

    refatoração: delegar a lógica de negócios do FuncionarioController para o FuncionarioService

--- backend/app/Http/Controllers/FuncionarioController.php
+++ backend/app/Http/Controllers/FuncionarioController.php
@@ -5,89 +5,42 @@ namespace App\Http\Controllers;
 use App\Models\Funcionario;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Services\FuncionarioService;
+use App\Http\Requests\FuncionarioRequest;
+
 
 class FuncionarioController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+    protected $funcionarioService;
+
+    public function __construct(FuncionarioService $funcionarioService)
+    {
+        $this->funcionarioService = $funcionarioService;
+    }
+
     public function index()
     {
-        $users = DB::select('select * from funcionarios');
-        return $users;
+        return $this->funcionarioService->index();
     }
-    
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
+
+    public function store(FuncionarioRequest $request)
     {
-        // return $request->all();
-        // $validated = $request->validate([
-        //     'nome' => 'required|string|max:255',
-        //     'setor' => 'required|string',
-        //     'matricula' => 'required|string',
-        //     'data_nascimento' => 'required|date',
-        //     'rg' => 'required|string',
-        //     'cpf' => 'required|string',
-        //     'pis_pasep' => 'required|string',
-        //     'titulo_eleitor' => 'required|string',
-        //     'mae' => 'required|string|max:255',
-        //     'bairro' => 'required|string|max:255',
-        //     'rua' => 'required|string|max:255',
-        //     'numero' => 'required|string|max:255',
-        //     'cidade' => 'required|string|max:255',
-        //     'uf' => 'required|string|max:255',
-        //     'cep' => 'required|string|max:255',
-        //     'estado_civil' => 'required|string|max:255',
-        //     'celular' => 'required|string|max:255',
-        //     'email' => 'required|string|email|max:255',
-        //     'carga_horaria' => 'required|string|max:255',
-        // ]);
-
-        $funcionario = Funcionario::firstOrNew(['matricula' => $request->matricula]);
-        if ($funcionario['id'] == null) {
-            $funcionario = Funcionario::create($request->all());
-            return response()->json([
-                'message' => 'Funcionário cadastrado com sucesso!',
-            ], 201);
-        } else {
-            return response()->json([
-                'message' => 'Funcionário já cadastrado!',
-            ], 200);
-        }
+        return $this->funcionarioService->criarFuncionario($request->all());
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show($id)
     {
-        $user = DB::select('select * from funcionarios where id = ?', [$id]);
-        return $user;
+        return $this->funcionarioService->procurarFuncionarioPeloId($id);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request)
+    public function update(FuncionarioRequest $request, $id)
     {
-        $funcionario = Funcionario::findOrFail($request->id);
-        $funcionario->update($request->all());
-        $funcionario->save();
-        return response()->json([
-            'message' => 'Funcionário atualizado com sucesso!',
-        ], 200);
+        return $this->funcionarioService->atualizarFuncionario($request->all(), $id);
     }
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Request $request)
+
+    public function destroy($id)
     {
-        $funcionario = Funcionario::findOrFail($request->id);
-        $funcionario->delete();
-        return response()->json([
-            'message' => 'Funcionário deletado com sucesso!',
-        ], 200);
+        return $this->funcionarioService->excluirFuncionario($id);
     }
+
 }
