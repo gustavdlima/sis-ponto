78ce1750177172f1b6dddb2fe33e5749c653df7b
gustavdlima <gustavdlima@gmail.com>
2024-09-23 15:26:53 -0300

    feat: implementar a lógica de negócios no FuncionarioService para operações de CRUD

---
+++ backend/app/Services/FuncionarioService.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Services;
+
+use App\Repositories\FuncionarioRepository;
+use Exception;
+
+class FuncionarioService
+{
+	protected $funcionarioRepository;
+
+	public function __construct(FuncionarioRepository $funcionarioRepository)
+	{
+		$this->funcionarioRepository = $funcionarioRepository;
+	}
+
+	public function index()
+	{
+		return $this->funcionarioRepository->all();
+	}
+
+	public function criarFuncionario(array $data)
+	{
+		$funcionario = $this->funcionarioRepository->firstOrNew(['matricula' => $data['matricula']]);
+		if ($funcionario['id'] == null) {
+			$funcionario = $this->funcionarioRepository->create($data);
+			return response()->json([
+				'message' => 'Funcionário cadastrado com sucesso!',
+			], 201);
+		} else {
+			throw new Exception('Funcionário já cadastrado!');
+		}
+	}
+
+	public function procurarFuncionarioPeloId($id)
+	{
+		if ($id == null)
+			throw new Exception('ID não informado!');
+
+		$funcionario = $this->funcionarioRepository->find($id);
+		if ($funcionario == null)
+			throw new Exception('Funcionário não encontrado!');
+
+		return $funcionario;
+	}
+
+	public function atualizarFuncionario(array $data, $id)
+	{
+		$funcionario = $this->funcionarioRepository->find($id);
+		if ($funcionario == null)
+			throw new Exception('Funcionário não encontrado!');
+
+		return $this->funcionarioRepository->update($data, $id);
+	}
+
+	public function excluirFuncionario($id)
+	{
+		$funcionario = $this->funcionarioRepository->find($id);
+		if ($funcionario == null)
+			throw new Exception('Funcionário não encontrado!');
+		$this->funcionarioRepository->delete($id);
+		return response()->json([
+			'message' => 'Funcionário deletado com sucesso!',
+		], 200);
+	}
+}
