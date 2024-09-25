52929d73545a20df5e06c5ba6501a0e7a35a2329
gustavdlima <gustavdlima@gmail.com>
2024-09-25 16:24:32 -0300

    feat: implementar FaltaRepository para lidar com operações do banco de dados

---
+++ backend/app/Repositories/FaltaRepository.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Falta;
+
+class FaltaRepository
+{
+	protected $model;
+
+	public function __construct(Falta $model)
+	{
+		$this->model = $model;
+	}
+
+	public function all()
+	{
+		return $this->model->all();
+	}
+
+	public function create(array $data)
+	{
+		return $this->model->create($data);
+	}
+
+	public function firstOrNew(array $data)
+	{
+		return $this->model->firstOrNew($data);
+	}
+
+	public function find($id)
+	{
+		return $this->model->find($id);
+	}
+
+	public function update(array $data, $id)
+	{
+		$falta = $this->model->find($id);
+		$falta->update($data);
+		return $falta;
+	}
+
+	public function delete($id)
+	{
+		return $this->model->destroy($id);
+	}
+	public function faltasFuncionario($id)
+    {
+        $faltas = Falta::where('id_funcionario', $id)->get();
+		
+        return response()->json($faltas);
+    }
+}
