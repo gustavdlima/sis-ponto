febe15e422d46de645ffcc7db281922b8eefebc5
gustavdlima <gustavdlima@gmail.com>
2024-09-26 15:01:05 -0300

    refactor: remove unnused parameters

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\{
-    PontoController, CadastroFuncionarioController, FuncionarioController, CargoController,
+    PontoController, FuncionarioController, CargoController,
     HorarioController, UserController, LoginController, RegistroController,
     JustificativaController, FaltaController, DiasDaSemanaController
 };
