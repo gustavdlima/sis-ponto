82d62eb71ab6bf914a3c6d1967fd72116280d754
gustavdlima <gustavdlima@gmail.com>
2024-10-09 16:42:37 -0300

    feat: Adiciona método para gerar relatório mensal de funcionários

---
+++ backend/app/Http/Controllers/RelatorioController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Repositories\RelatorioRepository;
+use Illuminate\Http\Request;
+use App\Services\FuncionarioService;
+
+class RelatorioController extends Controller
+{
+    protected $relatorioRepository;
+	protected $funcionarioService;
+
+    public function __construct(RelatorioRepository $relatorioRepository, FuncionarioService $funcionarioService)
+	{
+		$this->relatorioRepository = $relatorioRepository;
+		$this->funcionarioService = $funcionarioService;
+    }
+
+    public function gerarRelatorioDeRegistroDoPonto(Request $request)
+    {
+
+        $relatorio = $this->relatorioRepository->gerarRelatorio($request->mes, $request->id_funcionario);
+
+        return compact('relatorio');
+    }
+}
