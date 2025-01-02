65d1ab5537cd212e6f34f43d070ddf24388ada6c
gustavdlima <gustavdlima@gmail.com>
2025-01-02 10:57:56 -0300

    fix: script agora procura apenas os arquivos na pasta frontend

--- update_project.sh
+++ update_project.sh
@@ -19,25 +19,31 @@ section() {
   echo ""
 }
 
-# Substituir texto em todos os arquivos
+# Substituir texto nos arquivos dentro do diretório 'frontend'
 replace_text() {
-  local search_text=$1
-  local replace_text=$2
-  echo "Substituindo '$search_text' por '$replace_text' em todos os arquivos..."
-  find . -type f -exec sed -i "s|$search_text|$replace_text|g" {} \;
-  echo "Substituição concluída!"
-  echo "---------------------------------------------"
+  search_text="$1"
+  replace_text="$2"
+
+  echo "Procurando arquivos no diretório 'frontend' que contêm: $search_text..."
+  start_time=$(date +%s)
+
+  # Limita a busca ao diretório 'frontend' e seus subdiretórios
+  find frontend -type f -exec grep -l "$search_text" {} \; | xargs -I {} sed -i "s|$search_text|$replace_text|g" {}
+
+  end_time=$(date +%s)
+  duration=$((end_time - start_time))
+  echo "Substituição concluída em $duration segundos."
 }
 
 section "Atualizando repositório"
 time_command "git pull"
 
-section "Atualizando URLs nos arquivos"
+section "Atualizando URLs nos arquivos no diretório 'frontend'"
 replace_text "http://localhost:8000" "http://10.20.10.34:8000"
 
 section "Atualizando dependências do backend (Composer)"
 cd backend || exit
-time_command "sudo composer install"
+time_command "yes | sudo composer install"  # Automação do "yes" com sudo
 cd ..
 
 section "Atualizando dependências do frontend e construindo projeto"
